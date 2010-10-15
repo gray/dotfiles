6635d580a928789986cbcd69baf1479c21b637a4
gray <gray@cpan.org>
2010-10-15 12:56:03 -0700

    custom_movie_path works now

--- .flexget/plugins/plugin_custom_movie_path.py
+++ .flexget/plugins/plugin_custom_movie_path.py
@@ -8,6 +8,11 @@ class CustomMoviePath(object):
     Alters the path of the movie if:
         - english is not the primary language
         - it is a documentary
+
+    custom_movie_path:
+      default: /default/movie/path
+      documentary: /path/for/documentaries
+      foreign: /path/for/for/foreign/films
     """
 
     # Run after filter_imdb which has the default priority of 128
@@ -16,9 +21,10 @@ class CustomMoviePath(object):
     def validator(self):
         from flexget import validator
         config = validator.factory('dict')
-        # Don't specify these as paths as they might need to be created
-        config.accept('text', key='foreign', allow_replacement=True)
-        config.accept('text', key='documentary', allow_replacement=True)
+        # Don't specify these as paths as they might need to be (re)created
+        config.accept('text', key='default')
+        config.accept('text', key='documentary')
+        config.accept('text', key='foreign')
         return config
 
     def on_feed_modify(self, feed):
@@ -26,16 +32,22 @@ class CustomMoviePath(object):
             log.error('imdb plugin is not enabled on this feed. custom_movie_path only works with the imdb plugin.')
             return
 
-        foreign_path = feed.config.get('foreign')
-        documentary_path = feed.config.get('documentary')
+        config = feed.config.get('custom_movie_path', {})
+        default = config.get('default')
+        documentary = config.get('documentary')
+        foreign = config.get('foreign')
+        if not default and not documentary and not foreign:
+            return
 
         for entry in feed.accepted:
-            if foreign_path and 'english' == entry['imdb_languages'][0]:
-                entry['path'] = foreign_path
+            if foreign and 'english' != entry['imdb_languages'][0]:
+                entry['path'] = foreign
                 log.debug('%s is a foreign film' % (entry['imdb_name']))
                 continue
-            elif documentary_path and 'documentary' in entry['genres']:
-                entry['path'] = documentary_path
+            elif documentary and 'documentary' in entry['imdb_genres']:
+                entry['path'] = documentary
                 log.debug('%s is a documentary' % (entry['imdb_name']))
+            elif default:
+                entry['path'] = config.get('default')
 
 register_plugin(CustomMoviePath, 'custom_movie_path', debug=True)
