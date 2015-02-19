23dc0ee095c047613ea4bddc0de80a4cc2a2959e
gray <gray@cpan.org>
2015-02-18 20:06:15 -0800

    strict is implicitly enabled only for perl >= 5.11

--- bin/rsrc-textract.pl
+++ bin/rsrc-textract.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
 use 5.010;
+use strict;
 use warnings;
 
 use IPC::System::Simple qw(capture);
@@ -7,6 +8,6 @@ use IPC::System::Simple qw(capture);
 my $file = shift // die 'missing filename';
 exit unless -s "$file/..namedfork/rsrc";
 
-my $text = join '', map { (pack "H*", $_) =~ tr/\r/\n/r } map { split ' ' }
+my $text = join '', map { (pack "H*", $_) =~ tr/\r/\n/r } map split,
     capture(DeRez => qw(-only TEXT), $file) =~ m[^\t\$"([^"]+)"]gm;
 say $text;
